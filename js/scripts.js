72d45082665f26a34dd1bebda98dcec36f432535
Tori FavWest <practiceandthenpractice@gmail.com>
2021-03-29 10:12:43 -0700

    accept user input and display results in a different order using arrays > > Co-authored-by: Maxwell Meyer <maxreadswell@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
+$(document).ready(function(){
+  $("#favorites").submit(function(event) {
+    event.preventDefault();
+    let array = [];
+    array.push($("#input1").val());
+    array.push($("#input2").val());
+    array.push($("#input3").val());
+    array.push($("#input4").val());
+    console.log(array.toString());
+    let newAr = [];
+    newAr.push(array[1], array[0], array[2]);
+    console.log(newAr);
 
+    $(".favs").text(newAr.toString());
+    $(".results").fadeIn();
+  });
+});
